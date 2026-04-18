37048b67270856f6b38976a20b410153ac4c88b8
RLynx0 <luc.signin96@gmail.com>
2026-04-18 11:03:14 +0200

    Now cropping images

--- src/main.rs
+++ src/main.rs
@@ -1,29 +1,33 @@
 use std::{
     fs::{self, DirEntry},
     io::{Write, stdout},
-    path::PathBuf,
 };
 
-use image::ImageReader;
+use image::{DynamicImage, ImageReader};
 use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 fn main() -> anyhow::Result<()> {
-    println!(
-        "\r\x1b[K{:#?}",
-        fs::read_dir("./img")?
-            .collect::<Result<Vec<_>, _>>()?
-            .par_iter()
-            .map(process_entry)
-            .collect::<Result<Vec<_>, _>>()?
-    );
+    let cropped_images = fs::read_dir("./img")?
+        .collect::<Result<Vec<_>, _>>()?
+        .par_iter()
+        .map(process_entry)
+        .collect::<Result<Vec<_>, _>>()?;
+
+    println!("\r\x1b[KGot {} images", cropped_images.len());
     Ok(())
 }
 
-fn process_entry(entry: &DirEntry) -> anyhow::Result<(PathBuf, u32)> {
+fn process_entry(entry: &DirEntry) -> anyhow::Result<DynamicImage> {
     let path = entry.path();
     let image = ImageReader::open(&path)?.with_guessed_format()?.decode()?;
-    let dim = image.width().max(image.height());
+    let width = image.width();
+    let height = image.height();
+    let dim = width.max(height);
+
     print!("\r{path:?} : {dim}\x1b[K");
     stdout().flush()?;
-    Ok((path, dim))
+
+    let cropped = image.crop_imm((width - dim) / 2, (height - dim) / 2, dim, dim);
+
+    Ok(cropped)
 }
