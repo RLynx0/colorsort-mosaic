2abe1a5f94a73ff1fec8cc507454fa90d46a8e30
RLynx0 <luc.signin96@gmail.com>
2026-04-18 10:50:12 +0200

    Parallelized

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,29 @@
 use std::{
-    fs,
+    fs::{self, DirEntry},
     io::{Write, stdout},
+    path::PathBuf,
 };
 
 use image::ImageReader;
+use rayon::iter::{IntoParallelRefIterator, ParallelIterator};
 
 fn main() -> anyhow::Result<()> {
-    for p in fs::read_dir("./img")? {
-        let p = p?.path();
-        let image = ImageReader::open(&p)?.with_guessed_format()?.decode()?;
-        let dimension = image.width().max(image.height());
-        print!("\r{p:?} : {dimension}\x1B[K");
-        stdout().flush()?;
-    }
-    println!();
+    println!(
+        "\r\x1b[K{:#?}",
+        fs::read_dir("./img")?
+            .collect::<Result<Vec<_>, _>>()?
+            .par_iter()
+            .map(process_entry)
+            .collect::<Result<Vec<_>, _>>()?
+    );
     Ok(())
+}
+
+fn process_entry(entry: &DirEntry) -> anyhow::Result<(PathBuf, u32)> {
+    let path = entry.path();
+    let image = ImageReader::open(&path)?.with_guessed_format()?.decode()?;
+    let dim = image.width().max(image.height());
+    print!("\r{path:?} : {dim}\x1b[K");
+    stdout().flush()?;
+    Ok((path, dim))
 }
